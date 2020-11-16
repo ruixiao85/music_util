c92f713f54c9ea804e899f3b908c5a7c452e254e
ruixiao85 <ruixiao85@gmail.com>
2020-11-15 21:26:15 -0600

    band freq map to rounded int with weight.

--- foobar18band.py
+++ foobar18band.py
@@ -1,12 +1,6 @@
 #!/bin/env python
 from pathlib import Path
 import os
-bands=[55,77,110,156,220,311,440,622,880,1200,1800,2500,3500,5000,7000,10000,14000,20000]
-# static const double bands[] = {
-  # 65.406392,92.498606,130.81278,184.99721,261.62557,369.99442,523.25113,
-  # 739.9884 ,1046.5023,1479.9768,2093.0045,2959.9536,4186.0091,5919.9072,
-  # 8372.0181,11839.814,16744.036
-# };
 limit=20
 
 def parse_integer(value: str, default: int=0) -> int:
@@ -15,7 +9,8 @@ def parse_integer(value: str, default: int=0) -> int:
   except ValueError:
     return default
 
-def get_freq_resp(folder: str, ext: str=".csv")-> []:
+bands=[55,77,110,156,220,311,440,622,880,1200,1800,2500,3500,5000,7000,10000,14000,20000] # display
+def band_left(folder: str, ext: str=".csv")-> []:
   files=[f for f in os.listdir(folder) if f.endswith(ext)]
   print(files[0])
   with open(folder+os.path.sep+files[0]) as f:
@@ -24,7 +19,7 @@ def get_freq_resp(folder: str, ext: str=".csv")-> []:
     bi=0 # band index
     for line in f:
       cols=line.split(',')
-      cf=parse_integer(cols[0]) # current freqeuncy
+      cf=parse_integer(cols[0]) # current frequency
       cv=parse_integer(cols[1]) # current value
       if lf<bands[bi]<=cf:
         # print(f'{line}\tfreq{cf}\tval{cv}')
@@ -34,10 +29,43 @@ def get_freq_resp(folder: str, ext: str=".csv")-> []:
       lf=cf; lv=cv
   return ad
 
+# round(log(fq/55,base=1.414)) in [0,1,...,17]
+import math
+def freq_int_wt(fq:float)->(int,float):
+  fi=math.log(fq/55.0,1.414); fir=round(fi)
+  if 0<=fir<=17: return (fir,1.0-abs(fir-fi))
+  return (None,0)
+def band_int_round(folder: str, ext: str=".csv")-> []:
+  files=[f for f in os.listdir(folder) if f.endswith(ext)]
+  print(files[0])
+  iw_map={}
+  with open(folder+os.path.sep+files[0]) as f:
+    for line in f:
+      cols=line.split(',')
+      cf=parse_integer(cols[0],999999) # current frequency
+      ci,cw=freq_int_wt(cf) # current index, weight
+      if ci is not None: # valid index
+        cv=parse_integer(cols[1]) # current value
+        lv,lw=iw_map.get(ci,(0,0))
+        lv+=cv; lw+=cw
+        iw_map[ci]=(lv,lw)
+  # print(iw_map)
+  ad=[0]*18
+  for i in range(18):
+    if i in iw_map:
+      cv,cw=iw_map[i]
+      ad[i]=cv/cw
+  return ad
+
+# get_freq_resp=band_left
+get_freq_resp=band_int_round
+
 def clean_name(file: str)-> str:
   return file.replace("-","").replace(" ","").lower()
 
-myHps=["Audio-Technica ATH-W5000","Sennheiser HD 600", "Sennheiser HD 800","AKG K701"]
+# myHps=["Audio-Technica ATH-W5000","Sennheiser HD 600", "Sennheiser HD 800","AKG K701"]
+# myHps=["Audio-Technica ATH-W5000"]
+myHps=["Sennheiser HD 800","AKG K701"]
 myHpsClean=[clean_name(hp) for hp in myHps]
 myBrands=["Audio-Technica","Audeze","AKG","Beyerdynamic","Bose","Bowers","Denon","E-Mu","Focal","Fostex","Grado","HiFiMAN",
   "Massdrop","Meze","Monoprice","Monster","MrSpeakers","Oppo","Pioneer","Polk","Sennheiser","Shure","Sony","Stax","Ultrasone","ZMF"]
@@ -58,3 +86,4 @@ for sourceDir in ["headphonecom", "innerfidelity", "oratory1990"]:
               with open(f'{sourceDir}~{hpc}/{hpt}~{ratio}.feq'.replace(" ",""),"w") as f:
                 for a,b in zip(fc,ft):
                   f.write(f'{max(-1*limit,min(limit,round((b-a)*ratio)))}\n')
+            # exit(1) # debug one case
